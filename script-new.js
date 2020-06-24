c508c6058999cb065b921fdb75a39c21e7feb410
Rama Sabajo <rsabajo@gmail.com>
2020-06-24 11:17:30 +0200

    radiobuttons

--- script-new.js
+++ script-new.js
@@ -1,48 +1,50 @@
-const body = document.getElementById("body");
-const colorText = document.getElementById("color-main");
-const button = document.getElementById("button");
-const nav = document.getElementById("nav");
+const body = document.getElementById('body');
+const showColorText = document.getElementById('color-main');
+const button = document.getElementById('button');
+const nav = document.getElementById('nav');
 
-let colors = ["white", "red", "orange", "yellow", "green", "blue", "purple"];
-const colorIds = Array.from(document.getElementsByClassName("color-list-li"));
-const removeClass = (colors) =>
+let colors = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+const colorIds = Array.from(document.getElementsByClassName('color-list-li'));
+
+const removeClass = colors =>
   body.classList.remove(
-    "white",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "purple"
+    'white',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
   );
-const keyP = ["49", "50", "51", "52", "53", "54", "55"];
-const addClass = (color) => {
+const keyP = ['49', '50', '51', '52', '53', '54', '55'];
+
+const addClass = color => {
   body.classList.add(color);
 };
-const navOpen = button.addEventListener("mouseover", (e) => {
-  nav.classList.add("nav-open");
+const navOpen = button.addEventListener('mouseover', e => {
+  nav.classList.add('nav-open');
 });
-const navClick = button.addEventListener("click", (e) => {
-  nav.classList.toggle("nav-open");
+const navClick = button.addEventListener('click', e => {
+  nav.classList.toggle('nav-open');
 });
-const navHide = () => nav.classList.remove("nav-open");
+const navHide = () => nav.classList.remove('nav-open');
 const keyPress = () => {
   for (let i = 0; i < keyP.length; i++)
-    body.addEventListener("keypress", (e) => {
+    body.addEventListener('keypress', e => {
       if (e.keyCode == keyP[i]) {
         removeClass();
         addClass(colors[i]);
-        colorText.textContent = colors[i];
+        showColorText.textContent = colors[i];
       }
     });
 };
 keyPress();
 const clickColor = () => {
   for (let i = 0; i < colorIds.length; i++) {
-    colorIds[i].addEventListener("click", () => {
+    colorIds[i].addEventListener('click', () => {
       removeClass();
       addClass(colors[i]);
-      colorText.textContent = colors[i];
+      showColorText.textContent = colors[i];
       navHide();
     });
   }
